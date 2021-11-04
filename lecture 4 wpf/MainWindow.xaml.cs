30090eeb891de4e98ac043f60ac88a7f72213a1c
Furkan Gözükara <furkangozukara@gmail.com>
2021-11-04 15:56:12 +0300

    lecture 4 - lesson 2 v1

---
+++ lecture 4 wpf/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lecture_4_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        long irCounter = 0;
+        long irCounter2 = 0;
+        long irCounter3 = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Debug.WriteLine("thread id of main window : " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void btnExampleSingle_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("thread id of example button click : " + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 10000; i++)
+            {
+                lstNumbers.Items.Add(DateTime.Now.ToString("HH-mm-ss fff"));
+                var vrMs = new Random().Next(100, 900);
+                Debug.WriteLine($"i={i} , sleeping {vrMs}");
+                System.Threading.Thread.Sleep(vrMs);
+
+            }
+        }
+
+        private void btnSingleSubThread_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("thread id of btnSingleSubThread_Click : " + Thread.CurrentThread.ManagedThreadId);
+            var vrStartedTask = Task.Factory.StartNew(() =>
+            {
+                Debug.WriteLine("thread id of Task.Factory under  btnSingleSubThread_Click: " + Thread.CurrentThread.ManagedThreadId);
+                UpdateDates();
+
+            });
+        }
+
+        private void UpdateDates()
+        {
+            Debug.WriteLine("thread id of UpdateDates : " + Thread.CurrentThread.ManagedThreadId);
+            for (int i = 0; i < 10000; i++)
+            {
+                irCounter++;
+                //BeginInvoke non blocking
+                //Invoke blocking
+                Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    lstNumbers.Items.Add(DateTime.Now.ToString("HH-mm-ss fff") + " id: " + irCounter);
+                    Debug.WriteLine("thread id of Dispatcher : " + Thread.CurrentThread.ManagedThreadId);
+                }));
+
+                var vrMs = new Random().Next(100, 900);
+                Debug.WriteLine($"i={i} , sleeping {vrMs}");
+                System.Threading.Thread.Sleep(vrMs);
+
+            }
+        }
+
+        private void btnDataRacing_Click(object sender, RoutedEventArgs e)
+        {
+            irCounter = 0; irCounter2 = 0; irCounter3 = 0;
+            List<Task> lstTasks = new List<Task>();
+            for (int i = 0; i < 100; i++)
+            {
+                var vrStartedTask = Task.Factory.StartNew(() =>
+                {
+                    updateCounterValue();
+                   // updateCounterValueSync1();
+                   // updateCounterValueSync2();
+                });
+
+                lstTasks.Add(vrStartedTask);
+            }
+            Task.WaitAll(lstTasks.ToArray());
+            lstNumbers.Items.Add("counter 1 value: "+ irCounter.ToString("N0"));
+            lstNumbers.Items.Add("counter 2 value: " + irCounter2.ToString("N0"));
+            lstNumbers.Items.Add("counter 3 value: " + irCounter3.ToString("N0"));
+        }
+
+        void updateCounterValue()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                irCounter++;
+            }
+        }
+
+        void updateCounterValueSync1()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Interlocked.Increment(ref irCounter2);
+            }
+        }
+
+        private static object _lockCounter3 = new object();
+
+        void updateCounterValueSync2()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_lockCounter3)
+                {
+                    irCounter3++;
+                }
+                 
+            }
+        }
+    }
+}

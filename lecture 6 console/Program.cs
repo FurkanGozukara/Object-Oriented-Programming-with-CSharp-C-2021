715e1d47ecbd357aa7e84a3a70e28cbad9cdb9a3
Furkan Gözükara <furkangozukara@gmail.com>
2021-11-18 15:30:20 +0300

    lecture 6 part 3

--- lecture 6 console/Program.cs
+++ lecture 6 console/Program.cs
@@ -5,6 +5,7 @@ using static lecture_6_console.mathOps;
 
 namespace lecture_6_console
 {
+
     public static class csExtensions
     {
         //write an extension to test class that does print (test name + test name + square of test size)
@@ -18,6 +19,19 @@ namespace lecture_6_console
 
     public class Program
     {
+        public Program()
+        {
+
+        }
+
+        public Program(int gg)
+        {
+            irRead55 = gg;
+        }
+        //https://www.geeksforgeeks.org/difference-between-readonly-and-const-keyword-in-c-sharp/
+        const int irConst55 = 55;//compile time - static - never changes
+        readonly int irRead55 = 55;//run time - instance based - can be set at constructor
+
         private static int _MyProperty;//field
         public static int MyProperty
         {
@@ -67,10 +81,20 @@ namespace lecture_6_console
                 srTestName = "default test 2";
                 irTestSize = 100 + irAdd;
             }
+
+            public void saveToText//code this method
+
+                public void readFromText//code this method
         }
 
         static void Main(string[] args)
         {
+     
+
+            Console.WriteLine(irConst55);
+            Console.WriteLine(new Program().irRead55);
+            Console.WriteLine(new Program(66).irRead55);
+
             Console.WriteLine("Hello World! this is my property value= " + MyProperty);
             MyProperty = 50;
             Console.WriteLine("Hello World! this is my property value= " + MyProperty);
@@ -91,9 +115,51 @@ namespace lecture_6_console
             _test1.irTestSize = 300;
             _test1.printSpecial();
 
-            Console.WriteLine(new mathOps().sumNumbers(3,2,1,14,15,15,16));
+            Console.WriteLine(new mathOps().sumNumbers(3, 2, 1, 14, 15, 15, 16));
 
             Console.WriteLine(sumNumbers2(3, 2, 1, 14, 15, 15, 16));
+
+            for (int i = 0; i < 5; i++)
+            {
+                var vrValue = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(Convert.ToInt32(vrValue));
+
+                }
+                catch (FormatException E)
+                {
+                    Console.WriteLine("the value you have entered is incorrect integer format");
+                }
+                catch (OverflowException E)
+                {
+                    Console.WriteLine("the value you have entered for integer is too big or too small");
+                }
+                catch//catch all else
+                { }
+            }
+
+            while(true)
+            {
+                var vrValue = Console.ReadLine();
+                bool blBreak = false;
+                switch (vrValue)
+                {
+                    case "":
+                        blBreak = true;
+                        break;
+                    case "1":
+                    case "10":
+                        Console.WriteLine("1 or 10");
+                        break;
+                    case "100":
+                        Console.WriteLine("0");
+                        break;
+                }
+                if (blBreak)
+                    break;
+            }
+
         }
     }
 }
